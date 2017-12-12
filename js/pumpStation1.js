030fa3dfef41f53dc9733d87a6b0d72f6489090f
Kingder <kingder-c@users.noreply.github.com>
2017-12-12 14:16:35 +0800

    添加dat,gui

--- js/pumpStation1.js
+++ js/pumpStation1.js
@@ -1,11 +1,28 @@
-var scene, camera, renderer, daeModel;
+var scene, camera, renderer, daeModel1, daeModel2, daeModel3,daeModel7, daeModel8;
 
-//��ʼ������
+
+var Options = function () {
+    this.message = 'dat.gui';
+    this.speed = 0.8;
+    this.displayOutline = false;
+    this.button = function () { };
+};
+
+window.onload = function () {
+    var options = new Options();
+    var gui = new dat.GUI();
+
+    gui.add(options, 'message');
+    gui.add(options, 'speed', -5, 5);
+    gui.add(options, 'displayOutline');
+    gui.add(options, 'button');
+};
+//初始化场景
 function initScene() {
     scene = new THREE.Scene();
 }
 
-//��ʼ�������
+//初始化相机
 function initcamera() {
     aspect = 980 / 490;
     D = 6;
@@ -59,31 +76,73 @@ function initlight() {
 }
 
 function LoadModel() {
+    //一号水泵
+    var loader1 = new THREE.ColladaLoader();
+    loader1.load("model/maxdae/Pump.dae", function (collada) {
+        daeModel1 = collada.scene;
+        daeModel1.scale.set(0.3, 0.3, 0.3);
+        daeModel1.receiveShadow = true;
+        daeModel1.castShadow = true;
+
+        scene.add(daeModel1);
+    },
+        function (xhr) {
+            console.log((xhr.loaded / xhr.total * 100) + "% loaded");
+        }
+    );
+    //二号水泵
+    var loader2 = new THREE.ColladaLoader();
+    loader2.load("model/maxdae/Pump.dae", function (collada) {
+        daeModel2 = collada.scene;
+        daeModel2.scale.set(0.3, 0.3, 0.3);
+        daeModel2.receiveShadow = true;
+        daeModel2.castShadow = true;
+
+        scene.add(daeModel2);
+    },
+        function (xhr) {
+            console.log((xhr.loaded / xhr.total * 100) + "% loaded");
+        }
+    );
+    //三号水泵
+    var loader3 = new THREE.ColladaLoader();
+    loader3.load("model/maxdae/Pump.dae", function (collada) {
+        daeModel3 = collada.scene;
+        daeModel3.scale.set(0.3, 0.3, 0.3);
+        daeModel3.receiveShadow = true;
+        daeModel3.castShadow = true;
+
+        scene.add(daeModel3);
+    },
+        function (xhr) {
+            console.log((xhr.loaded / xhr.total * 100) + "% loaded");
+        }
+    );
         //泵站
     var loader7 = new THREE.ColladaLoader();
     loader7.load("model/maxdae/Pumpstation.DAE", function (collada) {
-        daeModel6 = collada.scene;
-        daeModel6.scale.set(0.3, 0.3, 0.3);
+        daeModel7 = collada.scene;
+        daeModel7.scale.set(0.3, 0.3, 0.3);
         //daeModel6.position.set(80, 80.7, 0);
         //daeModel6.rotation.x = 0.5 * Math.PI;
-        daeModel6.receiveShadow = true;
-        //daeModel6.castShadow = true;
+        daeModel7.receiveShadow = true;
+        daeModel7.castShadow = true;
         
-        scene.add(daeModel6);
+        scene.add(daeModel7);
     },
         function (xhr) {
             console.log((xhr.loaded / xhr.total * 100) + "% loaded");
         });
+    //水面
     var loader8 = new THREE.ColladaLoader();
     loader8.load("model/maxdae/water.DAE", function (collada) {
-        daeModel6 = collada.scene;
-        daeModel6.scale.set(0.3, 0.3, 0.3);
-        //daeModel6.position.set(80, 80.7, 0);
-        //daeModel6.rotation.x = 0.5 * Math.PI;
-        daeModel6.receiveShadow = true;
-        //daeModel6.castShadow = true;
+        daeModel8 = collada.scene;
+        daeModel8.scale.set(0.3, 0.3, 0.3);
 
-        scene.add(daeModel6);
+        daeModel8.receiveShadow = true;
+        daeModel8.castShadow = true;
+
+        scene.add(daeModel8);
     },
         function (xhr) {
             console.log((xhr.loaded / xhr.total * 100) + "% loaded");
@@ -116,91 +175,84 @@ function startGame() {
     initthree();
     initlight();
     LoadModel();
-    LoadMax();
+    //LoadMax();
     render();
     initControls();
-    /*$("#threeModelDiv").append("<label style='position:absolute;top:83px;left:566px;background-color:gray;border-color: white;border-style: groove;'><font color='#0000ff'>状态:正常</label> </button>");
-    $("#threeModelDiv").append("<label style='position:absolute;top:138px;left:569px;background-color:gray;border-color: white;border-style: groove;'><font color='#0000ff'>状态:正常</label> </button>") ;
-    $("#threeModelDiv").append("<label style='position:absolute;top:183px;left:572px;background-color:gray;border-color: white;border-style: groove;'><font color='#0000ff'>状态:正常</label> </button>") ;
-    $("#threeModelDiv").append("<label style='position:absolute;top:241px;left:577px;background-color:gray;border-color: white;border-style: groove;'><font color='#0000ff'>状态:正常</label> </button>") ;
-    $("#threeModelDiv").append("<label style='position:absolute;top:299px;left:582px;background-color:gray;border-color: white;border-style: groove;'><font color='#0000ff'>状态:正常</label> </button>") ;
-    $("#threeModelDiv").append("<label style='position:absolute;top:350px;left:586px;background-color:gray;border-color: white;border-style: groove;'><font color='#0000ff'>状态:正常</label> </button>") ;
-    $("#threeModelDiv").append("<label style='position:absolute;top:350px;left:586px;background-color:gray;border-color: white;border-style: groove;'><font color='#0000ff'>状态:正常</label> </button>");*/
 
 }
-function changePump(num, state) {
-    var pumpModel;
-    switch (num) {
-        case 1: pumpModel = daeModel1; break;
-        case 2: pumpModel = daeModel2; break;
-        case 3: pumpModel = daeModel3; break;
-        case 4: pumpModel = daeModel4; break;
-        case 5: pumpModel = daeModel5; break;
-        case 6: pumpModel = daeModel6; break;
+//function changePump(num, state) {
+//    var pumpModel;
+//    switch (num) {
+//        case 1: pumpModel = daeModel1; break;
+//        case 2: pumpModel = daeModel2; break;
+//        case 3: pumpModel = daeModel3; break;
+//        case 4: pumpModel = daeModel4; break;
+//        case 5: pumpModel = daeModel5; break;
+//        case 6: pumpModel = daeModel6; break;
 
-    }
-    switch (state) {
-        case "open": ControlPump.open(pumpModel); break;
-        case "close": ControlPump.close(pumpModel); break;
-        case "fault": ControlPump.fault(pumpModel); break;
-    }
-    //changecolor(pumpModel,state);
+//    }
+//    switch (state) {
+//        case "open": ControlPump.open(pumpModel); break;
+//        case "close": ControlPump.close(pumpModel); break;
+//        case "fault": ControlPump.fault(pumpModel); break;
+//    }
+//    //changecolor(pumpModel,state);
 
-}
-var ControlPump = {
-    open: function (model) {
-        for (var i = 0; i < model.children.length; i++) {
-            if (model.children[i].material)
-                model.children[i].material = new THREE.MeshPhongMaterial({ color: 0x00ff00 });
-            else
-                ControlPump.open(model.children[i]);
-        }
-    },
-    close: function (model) {
-        for (var i = 0; i < model.children.length; i++) {
-            if (model.children[i].material)
-                model.children[i].material = new THREE.MeshPhongMaterial({ color: 0x000000 });
-            else
-                ControlPump.close(model.children[i]);
-        }
-    },
-    fault: function (model) {
-        for (var i = 0; i < model.children.length; i++) {
-            if (model.children[i].material)
-                model.children[i].material = new THREE.MeshPhongMaterial({ color: 0xff0000 });
-            else
-                ControlPump.fault(model.children[i]);
-        }
-    }
-}
-function LoadMax() {
-    var onError = function (xhr) { };
-    THREE.Loader.Handlers.add(/\.dds$/i, new THREE.DDSLoader());
-    var mtlLoader = new THREE.MTLLoader();
-    mtlLoader.setPath('model/3DMAX');       //设置我们需要加载的mtl文件路径
-    mtlLoader.load('Pumpstatiom.mtl', function (material) {      //这里加载我们需要的文件名  
-        material.preload();
-        var objLoader = new THREE.OBJLoader();
-        objLoader.setMaterials(material);      //材质，也可自定义
-        objLoader.setPath('model/3DMAX');               //设置要加载的obj文件的路径
-        objLoader.load('Pumpstatiom.obj', function (object) {           //加载obj文件
-            object.position.z = 1;         //这里设置我们的素材相对于原来的大小以及旋转缩放等
-            object.position.y = -0.5;
-            object.scale.x = 0.2;
-            object.scale.y = 0.2;
-            object.scale.z = 0.2;
-            object1 = object;               //这里是对素材设置阴影的操作
-            for (var k in object.children) {  //由于我们的素材并不是看上去的一个整体，所以需要进行迭代
-                //对其中的所有孩子都设置接收阴影以及投射阴影
-                //才能看到阴影效果
-                object.children[k].castShadow = true;   //设置该对象可以产生阴影
-                object.children[k].receiveShadow = true;  //设置该对象可以接收阴影
-            }
-            scene.add(object1);
+//}
+//var ControlPump = {
+//    open: function (model) {
+//        for (var i = 0; i < model.children.length; i++) {
+//            if (model.children[i].material)
+//                model.children[i].material = new THREE.MeshPhongMaterial({ color: 0x00ff00 });
+//            else
+//                ControlPump.open(model.children[i]);
+//        }
+//    },
+//    close: function (model) {
+//        for (var i = 0; i < model.children.length; i++) {
+//            if (model.children[i].material)
+//                model.children[i].material = new THREE.MeshPhongMaterial({ color: 0x000000 });
+//            else
+//                ControlPump.close(model.children[i]);
+//        }
+//    },
+//    fault: function (model) {
+//        for (var i = 0; i < model.children.length; i++) {
+//            if (model.children[i].material)
+//                model.children[i].material = new THREE.MeshPhongMaterial({ color: 0xff0000 });
+//            else
+//                ControlPump.fault(model.children[i]);
+//        }
+//    }
+//}
+//function LoadMax() {
+//    var onError = function (xhr) { };
+//    THREE.Loader.Handlers.add(/\.dds$/i, new THREE.DDSLoader());
+//    var mtlLoader = new THREE.MTLLoader();
+//    mtlLoader.setPath('model/3DMAX');       //设置我们需要加载的mtl文件路径
+//    mtlLoader.load('Pumpstatiom.mtl', function (material) {      //这里加载我们需要的文件名  
+//        material.preload();
+//        var objLoader = new THREE.OBJLoader();
+//        objLoader.setMaterials(material);      //材质，也可自定义
+//        objLoader.setPath('model/3DMAX');               //设置要加载的obj文件的路径
+//        objLoader.load('Pumpstatiom.obj', function (object) {           //加载obj文件
+//            object.position.z = 1;         //这里设置我们的素材相对于原来的大小以及旋转缩放等
+//            object.position.y = -0.5;
+//            object.scale.x = 0.2;
+//            object.scale.y = 0.2;
+//            object.scale.z = 0.2;
+//            object1 = object;               //这里是对素材设置阴影的操作
+//            for (var k in object.children) {  //由于我们的素材并不是看上去的一个整体，所以需要进行迭代
+//                //对其中的所有孩子都设置接收阴影以及投射阴影
+//                //才能看到阴影效果
+//                object.children[k].castShadow = true;   //设置该对象可以产生阴影
+//                object.children[k].receiveShadow = true;  //设置该对象可以接收阴影
+//            }
+//            scene.add(object1);
 
-        }, onProgress, onError);
-    });
-}
+//        }, onProgress, onError);
+//    });
+//}
 
 
 
